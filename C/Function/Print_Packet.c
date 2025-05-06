7bdf9863f207fe8f02cab7ee8e07ca670b096e0d
An sejoon <156414896+ansejoon00@users.noreply.github.com>
2025-05-06 23:59:50 +0900

    Create Print_Packet.c

---
+++ C/Function/Print_Packet.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+void printf_byte(const char *label, uint8_t byte_val) {
+    if (DEBUG) {
+        printf("  [%s] [1]\n", label);
+        printf("     %02X\n", byte_val);
+    }
+}
+
+void printf_byte(const char *label, const uint8_t *array, int start_index, int size) {
+    if (DEBUG) {
+        printf("  [%s] [%d]", label, size);
+        for (int i = 0; i < size; i++) {
+            if (i % 25 == 0)
+                printf("\n    ");
+            printf(" %02X", array[start_index + i]);
+        }
+        printf("\n");
+    }
+}
+
+void printf_byte_s(const char *label, const uint8_t *array, int start_index, int size) {
+    printf("  [%s] [%d]", label, size);
+    for (int i = 0; i < size; i++) {
+        if (i % 25 == 0)
+            printf("\n    ");
+        printf(" %02X", array[start_index + i]);
+    }
+    printf("\n");
+}
+
+void printf_Send_packet(const uint8_t *packet, int size) {
+    printf("[Send Packet Data Size] : %d\n", size);
+    printf_byte_s("Send Packet Data", packet, 0, size);
+}
+
+void printf_Trap_packet(const uint8_t *packet, int size) {
+    printf("[Trap Packet Data Size] : %d\n", size);
+    printf_byte_s("Trap Packet Data", packet, 0, size);
+}
+
+void printf_Recv_packet(const uint8_t *packet, int size) {
+    printf("[Recv Packet Data Size] : %d\n", size);
+    printf_byte_s("Recv Packet Data", packet, 0, size);
+}
