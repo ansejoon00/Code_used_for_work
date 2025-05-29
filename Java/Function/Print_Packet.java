298daa87b449550f787c7b0aee36c01a0c745315
An sejoon <156414896+ansejoon00@users.noreply.github.com>
2025-05-29 18:05:15 +0900

    Update Print_Packet.java

--- Java/Function/Print_Packet.java
+++ Java/Function/Print_Packet.java
@@ -32,18 +32,57 @@ public class Print_Packet {
         System.out.println();
     }
 
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSSSSS");
+
+    private static String get_Formatted_Time() {
+        return LocalDateTime.now().format(formatter);
+    }
+
+    protected static void printf_Send_cmd(char cmd, boolean result) {
+        String time = get_Formatted_Time();
+        if (result) {
+            System.out.println("[Send Packet] Send Success CMD: " + cmd + " / Time : " + time);
+        } else {
+            System.out.println("==================================================================================");
+            System.out.println("[Send Packet] Send CMD : " + cmd + " /  Time : " + time);
+        }
+    }
+
     protected static void printf_Send_packet(byte[] packet) {
-        System.out.println("[Send Packet Data Size] : " + packet.length);
-        Print_Packet.printf_byte_s("Send Packet Data", packet, 0, packet.length);
+        String time = get_Formatted_Time();
+        System.out.println("[Send Packet Data Size] : " + packet.length + " / Time : " + time);
+        printf_byte_s("Send Packet Data", packet, 0, packet.length);
+    }
+
+    protected static void printf_Trap_cmd(char cmd, boolean result) {
+        String time = get_Formatted_Time();
+        if (result) {
+            System.out.println("[Trap Packet] Trap Success CMD: " + cmd + " / Trap Time : " + time);
+        } else {
+            System.out.println("==================================================================================");
+            System.out.println("[Trap Packet] Trap CMD : " + cmd + " / Trap Time : " + time);
+        }
     }
 
     protected static void printf_Trap_packet(byte[] packet) {
-        System.out.println("[Trap Packet Data Size] : " + packet.length);
-        Print_Packet.printf_byte_s("Trap Packet Data", packet, 0, packet.length);
+        String time = get_Formatted_Time();
+        System.out.println("[Trap Packet Data Size] : " + packet.length + " / Time : " + time);
+        printf_byte_s("Trap Packet Data", packet, 0, packet.length);
+    }
+
+    protected static void printf_Recv_cmd(char cmd, boolean result) {
+        String time = get_Formatted_Time();
+        if (result) {
+            System.out.println("[Recv Packet] Recv Success CMD: " + cmd + " / Recv Time : " + time);
+        } else {
+            System.out.println("==================================================================================");
+            System.out.println("[Recv Packet] Recv CMD : " + cmd + " / Recv Time : " + time);
+        }
     }
 
     protected static void printf_Recv_packet(byte[] packet) {
-        System.out.println("[Recv Packet Data Size] : " + packet.length);
-        Print_Packet.printf_byte_s("Recv Packet Data", packet, 0, packet.length);
+        String time = get_Formatted_Time();
+        System.out.println("[Recv Packet Data Size] : " + packet.length + " / Time : " + time);
+        printf_byte_s("Recv Packet Data", packet, 0, packet.length);
     }
 }
